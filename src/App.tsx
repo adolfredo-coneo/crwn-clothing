61ceadde90964616323dd158c2872f2b1a010d87
Adolfredo Coneo <ado.coneo@gmail.com>
2022-11-03 22:33:01 -0500

    add firebase

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { Routes, Route } from 'react-router-dom';
 import NavigationBar from './components/navigation-bar/navigation-bar.component';
 import HomePage from './pages/home/home.page';
 import ShopPage from './pages/shop/shop.page';
+import SignInPage from './pages/sign-in/sign-in.page';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Route path="/" element={<NavigationBar />}>
         <Route index element={<HomePage />} />
         <Route path="shop" element={<ShopPage />} />
+        <Route path="sign-in" element={<SignInPage />} />
       </Route>
     </Routes>
   );
---
+++ src/pages/sign-in/sign-in.page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+type Props = {}
+
+const SignInPage = (props: Props) => {
+  return (
+    <div><h1>Sign in Page</h1></div>
+  )
+}
+
+export default SignInPage
