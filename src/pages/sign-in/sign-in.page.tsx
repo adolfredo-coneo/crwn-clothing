ca9f4767874f0295ff4a401fe95cfb9967c5c420
Adolfredo Coneo <ado.coneo@gmail.com>
2022-11-06 14:39:43 -0500

    add sign up with redirect

--- src/pages/sign-in/sign-in.page.tsx
+++ src/pages/sign-in/sign-in.page.tsx
@@ -1,23 +1,45 @@
-import React from 'react';
-import { signInWithGooglePopup, createUserDocumentFromAuth } from '../../utils/firebase.utils';
+import React, { useEffect } from 'react';
+import { getRedirectResult, UserCredential } from 'firebase/auth';
+
+import {
+  auth,
+  signInWithGooglePopup,
+  signInWithGoogleRedirect,
+  createUserDocumentFromAuth,
+} from '../../utils/firebase.utils';
 
 type Props = {};
 
+const createUser = async (response: UserCredential | null) => {
+  if (response) {
+    console.log(response);
+    await createUserDocumentFromAuth(response.user);
+  } else {
+    console.log('No user');
+  }
+};
+
 const SignInPage = (props: Props) => {
+  useEffect(() => {
+    const getRedirect = async () => {
+      const result = await getRedirectResult(auth);
+      await createUser(result);
+    };
+    getRedirect();
+  }, []);
+
   const logGoogleUser = async () => {
     const response = await signInWithGooglePopup();
-    if (response) {
-      console.log(response);
-      const userDocument = await createUserDocumentFromAuth(response.user);
-    } else {
-      console.log('No user');
-    }
+    await createUser(response);
   };
 
   return (
     <div>
       <h1>Sign in Page</h1>
-      <button onClick={logGoogleUser}>Sign in with Google</button>
+      <button onClick={logGoogleUser}>Sign in with Google PopUp</button>
+      <button onClick={signInWithGoogleRedirect}>
+        Sign in with Google Redirect
+      </button>
     </div>
   );
 };
